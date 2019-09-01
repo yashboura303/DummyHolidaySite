26167a39afabea8e03230cbf8c4feb1249c3d8ac
yashboura303 <yashboura303@gmail.com>
2019-09-01 13:10:15 +0530

    fixed image carousel

--- index.js
+++ index.js
@@ -1,68 +1,37 @@
-
-
-
-for ( let i = 0; i<99;i++){
+// const uuidv4 = require('uuid/v4')
+for ( let i = 0; i<300;i = i + 3){
     
-    var cardTemplate = document.getElementById("rowDiv").cloneNode(true);
-    
-    cardTemplate.childNodes[1].childNodes[3].setAttribute("id", `sss${i+1}`);
-    cardTemplate.childNodes[3].childNodes[3].setAttribute("id", `sss${i+2}`);
-    cardTemplate.childNodes[5].childNodes[3].setAttribute("id", `sss${i+3}`);
+    let cardTemplate = document.getElementById("rowDiv").cloneNode(true);
+    div1 = cardTemplate.querySelector('#carouselExampleControls1')
+    div2 = cardTemplate.querySelector('#carouselExampleControls2')
+    div3 = cardTemplate.querySelector('#carouselExampleControls3')
+    a1 = div1.querySelector('.carousel-control-prev')
+    a2 = div1.querySelector('.carousel-control-next')
+    b1 = div2.querySelector('.carousel-control-prev')
+    b2 = div2.querySelector('.carousel-control-next')
+    c1 = div3.querySelector('.carousel-control-prev')
+    c2 = div3.querySelector('.carousel-control-next')
+    div1.setAttribute('id', `sss${i}`);
+    div2.setAttribute('id', `sss${i+1}`);
+    div3.setAttribute('id', `sss${i+2}`);
+    a1.setAttribute('href',`#sss${i}`)
+    a2.setAttribute('href',`#sss${i}`)
+    b1.setAttribute('href',`#sss${i+1}`)
+    b2.setAttribute('href',`#sss${i+1}`)
+    c1.setAttribute('href',`#sss${i+2}`)
+    c2.setAttribute('href',`#sss${i+2}`)
 
+    cardTemplate.setAttribute('id','');    
 
-    cardTemplate.childNodes[1].childNodes[3].childNodes[5].setAttribute("href", `#sss${i+1}`)
-    cardTemplate.childNodes[1].childNodes[3].childNodes[7].setAttribute("href", `#sss${i+1}`)
-    cardTemplate.childNodes[3].childNodes[3].childNodes[5].setAttribute("href", `#sss${i+2}`)
-    cardTemplate.childNodes[3].childNodes[3].childNodes[7].setAttribute("href", `#sss${i+2}`)
-    cardTemplate.childNodes[5].childNodes[3].childNodes[5].setAttribute("href", `#sss${i+3}`)
-    cardTemplate.childNodes[5].childNodes[3].childNodes[7].setAttribute("href", `#sss${i+3}`)
-    
-
-    var mainDivv = document.getElementById("mainDiv");
+    let mainDivv = document.getElementById("mainDiv");
     mainDivv.appendChild(cardTemplate);
 
-    
-    // var mainDiv = cardTemplatee.childNodes;
-    // var newDiv1 = mainDiv[1];
-    // var newDiv2 = mainDiv[3];
-    // var newDiv3 = mainDiv[5];
-
-
-    // var carousel1 = newDiv1.childNodes
-    // var carousel2 = newDiv2.childNodes
-    // var carousel3 = newDiv3.childNodes
-
-    
-
-    // carousel1[3].setAttribute("id",`sss${i+1}`);
-    // carousel2[3].setAttribute("id",`sss${i+2}`);
-    // carousel3[3].setAttribute("id",`sss${i+3}`);
-
-
- 
-
-    // var a1 = carousel1[3].childNodes
-    // var a2 = carousel2[3].childNodes
-    // var a3 = carousel3[3].childNodes
-    // console.log(a1)
-    // console.log(a2)
-    // console.log(a3)
-    // var a11 = a1[5]
-    // var a21 = a1[7]
-    // var a12 = a2[5]
-    // var a22 = a2[7]
-    // var a13 = a3[5]
-    // var a23 = a3[7]
-
-    // a11.setAttribute("href",`#sss${i+1}`)
-    // a12.setAttribute("href",`#sss${i+2}`)
-    // a13.setAttribute("href",`#sss${i+3}`)
-    // a21.setAttribute("href",`#sss${i+1}`)
-    // a22.setAttribute("href",`#sss${i+2}`)
-    // a23.setAttribute("href",`#sss${i+3}`)
-    
-
-    
 }
+    
+    
+// let x= uuidv4();
+// console.log(x)
+    
+
 
 
